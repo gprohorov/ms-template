802b598efa1738bfa97db7b4c4d0d740b99e794c
gprohorov <g.prokhorov@indevlab.com>
2023-10-30 18:30:42 +0200

    eureka settings

---
+++ dean/src/main/java/edu/pro/dean/DeanApplication.java
@@ -0,0 +1,13 @@
+package edu.pro.dean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeanApplication.class, args);
+	}
+
+}
---
+++ dean/src/test/java/edu/pro/dean/DeanApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pro.dean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
